3316f13f53700a6fa388a7a3af7d1a588d95e2b7
luoguangming <luoguangming@imlianai.com>
2019-02-20 10:47:59 +0800

    add libs

--- app/src/test/java/com/alien/gradletest/ExampleUnitTest.kt
+++ app/src/test/java/com/alien/gradletest/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.alien.gradletest
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.mindrot.jbcrypt.BCrypt
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,14 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+
+    @Test
+    fun getCode() {
+        val hashed = BCrypt.hashpw("admin123", BCrypt.gensalt())
+        System.out.println(hashed)
+        if (BCrypt.checkpw("admin123", hashed))
+            System.out.println("It matches")
     }
 }
